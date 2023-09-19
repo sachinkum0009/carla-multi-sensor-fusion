8a768974ac52cb1ffea6e28ee0583e92774a403a
Sachin Kumar <sachinkum123567@gmail.com>
2023-09-19 20:16:32 +0200

    gps odom publisher added

--- multi_sensor_fusion/include/multi_sensor_fusion/gps_odom.hpp
+++ multi_sensor_fusion/include/multi_sensor_fusion/gps_odom.hpp
@@ -2,16 +2,37 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/nav_sat_fix.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <cmath>
 
 namespace gps_odom
 {
     class GpsOdom : public rclcpp::Node {
+    // Define constants for Mercator projection
+    const double EARTH_RADIUS = 6371000.0; // Earth's radius in meters
+    const double METERS_PER_DEGREE = EARTH_RADIUS * M_PI / 180.0;
 public:
     GpsOdom();
     ~GpsOdom();
 private:
     rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr _gnssSub;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr _gpsOdomPub;
+
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    nav_msgs::msg::Odometry _odomMsg;
+
+    /**
+     * Function used to publish the odometry
+    */
+    void publishOdom();
 
     void gnssCallback(const sensor_msgs::msg::NavSatFix::SharedPtr msg);
+    /**
+     * code to conver the latitude and longitude
+     * 
+     * to x y coordinates
+    */
+    void convertLatLng2XYCoord(const double lat, const double lng, double& x, double& y);
     };
 } // namespace gps_odom
--- multi_sensor_fusion/src/gps_odom.cpp
+++ multi_sensor_fusion/src/gps_odom.cpp
@@ -4,14 +4,30 @@ namespace gps_odom
 {
     GpsOdom::GpsOdom() : Node("gps_odom") {
         _gnssSub = this->create_subscription<sensor_msgs::msg::NavSatFix>("/carla/ego_vehicle/gnss", 10, std::bind(&GpsOdom::gnssCallback, this, std::placeholders::_1));
+        _gpsOdomPub = this->create_publisher<nav_msgs::msg::Odometry>("/gps_odom", 10);
+        _odomMsg.header.frame_id = "odom";
+        _odomMsg.child_frame_id = "base_link";
+        timer_ = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&GpsOdom::publishOdom, this));
     }
     GpsOdom::~GpsOdom() {
 
     }
     void GpsOdom::gnssCallback(const sensor_msgs::msg::NavSatFix::SharedPtr msg) {
         RCLCPP_INFO(this->get_logger(), "Received Gnns Msg");
-
+        convertLatLng2XYCoord(msg->latitude, msg->longitude, _odomMsg.pose.pose.position.x, _odomMsg.pose.pose.position.y);
     }
+
+    void GpsOdom::convertLatLng2XYCoord(const double lat, const double lng, double& x, double& y) {
+        // Convert latitude and longitude to meters using Mercator projection
+        x = lng * METERS_PER_DEGREE;
+        y = log(tan((90.0 + lat) * M_PI / 360.0)) * EARTH_RADIUS;
+    }
+
+    void GpsOdom::publishOdom() {
+        _odomMsg.header.stamp = this->get_clock()->now();
+        _gpsOdomPub->publish(_odomMsg);
+    }
+
 
 } // namespace gps_odom
 
