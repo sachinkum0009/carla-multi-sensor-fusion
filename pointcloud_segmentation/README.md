69b1972234372e66e8f4be1ad848feceacfc26e8
Sachin Kumar <sachinkum123567@gmail.com>
2024-07-07 22:45:57 +0200

    :zap: node updated to use pointcloud segmentation using dbscan

---
+++ pointcloud_segmentation/README.md
@@ -0,0 +1,13 @@
+# Pointcloud Segmentation
+
+## Commands
+
+```bash
+colcon build --packages-select pointcloud_segmentation
+
+ros2 run pointcloud_segmentation pointcloud_segmentation_node
+```
+
+## Results
+
+![Pointcloud2 Segmentation2 Rviz2](media/pointcloud2_rviz2.png)
--- pointcloud_segmentation/pointcloud_segmentation/pointcloud_segmentation_node.py
+++ pointcloud_segmentation/pointcloud_segmentation/pointcloud_segmentation_node.py
@@ -6,48 +6,81 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image, PointCloud2
 from sensor_msgs_py import point_cloud2 as pc2
 
-from cv_bridge import CvBridge, CvBridgeError
-
+from sklearn.cluster import DBSCAN
 import numpy as np
 import time
+from matplotlib import pyplot as plt
 
-import open3d as o3d
 
 class PointCloudSegmentationNode(Node):
     def __init__(self):
         super().__init__("pointcloud_segmentation_node")
         self.img_sub = self.create_subscription(PointCloud2, "/velodyne_points", self.pointcloud_cb, 10)
         self.pointcloud_segment_pub = self.create_publisher(PointCloud2, "pointcloud_semantic_points", 10)
+        self.db_scan = DBSCAN(eps=0.4, min_samples=20)
+        self.get_logger().info("node started")
 
     def pointcloud_cb(self, msg: PointCloud2):
         self.get_logger().info("received point cloud data")
         # pts = pc2.read_points(msg, field_names=["x", "y", "z"], skip_nans=True)
         pts = self.pointcloud2_to_array(msg)
         
-        clustered_pts = self.segment_pointcloud(list(pts))
+        colors = self.segment_pointcloud(pts)
         self.get_logger().info("clustered point cloud data")
+        self.array_to_pointcloud2(pts, colors)
         
     def pointcloud2_to_array(self, cloud_msg):
-        points = list(pc2.read_points(cloud_msg, field_names=["x", "y", "z"], skip_nans=True))
-        return np.array(points, dtype=np.float32)
+        points = pc2.read_points(cloud_msg, field_names=["x", "y", "z"], skip_nans=True)
+        return np.vstack([points['x'], points['y'], points['z']])
 
-    def segment_pointcloud(self, points):
-        # Convert numpy array to Open3D point cloud
-        cloud = o3d.geometry.PointCloud()
-        cloud.points = o3d.utility.Vector3dVector(points)
-        
-        # Segment the point cloud using Euclidean clustering
-        labels = np.array(cloud.cluster_dbscan(eps=0.02, min_points=10, print_progress=True))
-        
-        # Separate the points into clusters
-        max_label = labels.max()
-        self.get_logger().info(f"point cloud segmented into {max_label + 1} clusters")
-        
-        # For simplicity, return points of the largest cluster
-        largest_cluster_idx = np.argmax(np.bincount(labels[labels >= 0]))
-        largest_cluster_points = points[labels == largest_cluster_idx]
-        
-        return largest_cluster_points
+    def segment_pointcloud(self, points: np.ndarray) -> np.ndarray:
+        self.db_scan.fit(points.T)
+        labels = self.db_scan.labels_
+
+        # Number of clusters in labels, ignoring noise if present.
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+        n_noise_ = list(labels).count(-1)
+
+        print("Estimated number of clusters: %d" % n_clusters_)
+        print("Estimated number of noise points: %d" % n_noise_)
+
+        # Define colors for each cluster label
+        colors = np.zeros((len(labels), 1))  # Initialize colors array
+        unique_labels = set(labels)
+        for i, label in enumerate(unique_labels):
+            if label == -1:
+                colors[labels == label] = [0]  # Noise points in black
+            else:
+                colors[labels == label] = label # plt.cm.Spectral(i / len(unique_labels))[:1]  # Assign color based on label
+
+        return colors
+    
+    def array_to_pointcloud2(self, points: np.ndarray, colors: np.ndarray) -> PointCloud2:
+        stamp = self.get_clock().now().to_msg()
+        frame_id = 'base_scan'
+        # pc = pc2.create_cloud_xyz32(pc2.Header(stamp=stamp, frame_id=frame_id), points)
+
+        header = pc2.Header()
+        header.frame_id = frame_id
+        header.stamp = stamp
+
+        # print(colors.T.shape)
+        # print(points.shape)
+
+        points = np.vstack([points, colors.T])
+
+        # print(points.shape)
+        # print(len(points))
+
+        fields =[pc2.PointField(name='x',  offset=0, datatype=pc2.PointField.FLOAT32, count = 1),
+                pc2.PointField(name='y',  offset=4, datatype=pc2.PointField.FLOAT32, count = 1),
+                pc2.PointField(name='z',  offset=8, datatype=pc2.PointField.FLOAT32, count = 1),
+                pc2.PointField(name='intensity',  offset=12, datatype=pc2.PointField.FLOAT32, count = 1)
+                ]
+        pc = pc2.create_cloud(header, fields, points.T.astype(np.float32) )
+        self.pointcloud_segment_pub.publish(pc)
+
+
 
 def main(args=None):
     rclpy.init(args=args)
